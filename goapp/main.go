06eb1b1963ef67bb85a44c441c933e53f345e80a
Jakub Bujny <jakub.bujny@ac-project.net>
2018-07-25 22:12:23 +0200

    completed goapp

--- goapp/main.go
+++ goapp/main.go
@@ -9,19 +9,29 @@ import (
 	"log"
 	"text/template"
 	"io"
+	"io/ioutil"
+	"time"
+	"bytes"
+	"strconv"
+	"encoding/base64"
+	"strings"
 )
 
-//https://davidwalsh.name/browser-camera
+//source: https://davidwalsh.name/browser-camera
 const site_template string = `
 <html>
 <body>
 <video id="video" width="640" height="480" autoplay></video><br />
 <button id="snap">Snap&Upload</button><br />
-<canvas id="canvas" width="640" height="480"></canvas>
+<canvas id="canvas" width="640" height="480" style="display: none"></canvas>
 <br /><br />
 {{ range $key, $value := . }}
-   <li>{{ $value.Key }}</li>
+   <li><a href="/image/{{ $value.Key }}">{{ $value.Key }}</a></li>
 {{ end }}
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
 <script>
 var video = document.getElementById('video');
 
@@ -36,9 +46,26 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var video = document.getElementById('video');
 
-// Trigger photo take
 document.getElementById("snap").addEventListener("click", function() {
 	context.drawImage(video, 0, 0, 640, 480);
+	$.ajax({
+        url: '/image',
+        type: 'POST',
+        data: canvas.toDataURL("image/jpeg"),
+        success: function (data) {
+            if (data.error) {
+                alert(data.error);
+                return;
+            }
+			location.reload();
+        }, 
+        error : function (r, s, e) {
+                alert("Unexpected error:" + e);
+                console.log(r);
+                console.log(s);
+                console.log(e);
+            } 
+        });
 });
 </script>
 </body>
@@ -81,13 +108,26 @@ func GetImage(w http.ResponseWriter, r *http.Request) {
 }
 
 func SaveImage(w http.ResponseWriter, r *http.Request) {
-
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	svc := SpawnAwsSdkS3Service()
+	decoded, err := base64.StdEncoding.DecodeString(strings.Split(string(body), ",")[1])
+	if err != nil {
+		panic(err)
+	}
+	_ , err1 := svc.PutObject(&s3.PutObjectInput{Key: aws.String(strconv.FormatInt(time.Now().UnixNano(), 10)+".jpg"), Bucket: aws.String("jakubbujny-article-form-your-cloud-1-photos"), Body: bytes.NewReader(decoded)})
+	if err1 != nil {
+		panic(err1)
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", GetRootSite).Methods("GET")
 	router.HandleFunc("/image/{image_name}", GetImage).Methods("GET")
-	router.HandleFunc("/image/{image_name}", SaveImage).Methods("POST")
+	router.HandleFunc("/image", SaveImage).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
